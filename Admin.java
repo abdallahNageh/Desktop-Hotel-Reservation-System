0eec1980640544eb3c0b56594c41f312c8b8951c
samehamged59-svg <samehamged59@gmail.com>
2026-04-09 19:36:14 +0200

    Add files via upload

--- Admin.java
+++ Admin.java
@@ -1,20 +1,40 @@
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
-
+import java.util.ArrayList;
 public class Admin extends Staff {
-
-    private List<String> permissions;
-
-    public Admin(String username, String password, Date dateOfBirth, int workingHours, List<String> permissions) {
-        super(username, password, dateOfBirth, workingHours);
-        this.permissions = permissions;
+    public Admin(String username, String password, LocalDate dateOfBirth, int workingHours) {
+        super(username, password, dateOfBirth, workingHours,Role.ADMIN);
+    }
+    public void addRoom(Room room) {
+        HotelDatabase.addRoom(room);
     }
 
-    public List<String> getPermissions() {
-        return permissions;
+    public void removeRoom(Room room) {
+        HotelDatabase.removeRoom(room);
     }
 
-    public void setPermissions(List<String> permissions) {
-        this.permissions = permissions;
+    // 🔹 CRUD RoomTypes
+    public void addRoomType(RoomType type) {
+        HotelDatabase.addRoomType(type);
     }
-}
+
+    public void removeRoomType(RoomType type) {
+        HotelDatabase.getRoomTypes().remove(type);
+    }
+    public void addAmenity(Amenity amenity) {
+        HotelDatabase.addAmenity(amenity);
+    }
+
+    public void removeAmenity(Amenity amenity) {
+        HotelDatabase.removeAmenity(amenity);
+    }
+
+    }
+
+
+
+
+
+
+
+
