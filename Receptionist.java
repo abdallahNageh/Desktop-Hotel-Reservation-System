e2ed0118ef8479804e04c3dc2ede3dd9e9036368
Beshoyhany312 <beshoyhany312@gmail.com>
2026-04-09 19:20:47 +0200

    Add files via upload

--- Receptionist.java
+++ Receptionist.java
@@ -1,66 +1,3 @@
-//import java.time.LocalDate;
-//import java.util.ArrayList;
-//public class Receptionist extends Staff{
-//    private String department;
-//
-//    public Receptionist(String username, String password, LocalDate dateOfBirth, int workingHours, String department) {
-//        super(username, password,dateOfBirth,workingHours,Role.RECEPTIONIST);
-//        this.department = department;
-//    }
-//
-//    public String getDepartment() {
-//        return department;
-//    }
-//
-//    public void setDepartment(String department) {
-//        this.department = department;
-//    }
-//    
-//    public void manageCheckIn(Guest guest) {
-//    for (Reservation r : Database.reservations) {
-//        if (r.getGuest().equals(guest)) {
-//            
-//            if (r.getStatus() == ReservationStatus.CONFIRMED) {
-//                System.out.println("Guest checked in successfully.");
-//                // optional: mark room unavailable
-//                r.getRoom().setAvailable(false);
-//                return;
-//            }
-//        }
-//    }
-//    System.out.println("No valid reservation found for check-in.");
-//}
-//    
-//    public void manageCheckOut(Guest guest) {
-//    for (Reservation r : Database.reservations) {
-//        if (r.getGuest().equals(guest) && 
-//            r.getStatus() == ReservationStatus.CONFIRMED) {
-//
-//            // Generate invoice
-//            Invoice invoice = new Invoice(r);
-//            Database.invoices.add(invoice);
-//
-//            // Complete reservation
-//            r.setStatus(ReservationStatus.COMPLETED);
-//
-//            // Make room available again
-//            r.getRoom().setAvailable(true);
-//
-//            System.out.println("Check-out successful. Invoice generated.");
-//            return;
-//        }
-//    }
-//    System.out.println("No active reservation found.");
-//}
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//}
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -81,17 +18,7 @@ public class Receptionist extends Staff {
         this.department = department;
     }
 
-//    // ✅ View Guests
-//    public ArrayList<Guest> viewGuests() {
-//        return HotelDatabase.getGuests();
-//    }
-//
-//    // ✅ View Reservations
-//    public ArrayList<Reservation> viewReservations() {
-//        return HotelDatabase.getReservations();
-//    }
-
-    // ✅ Check-In
+    // Check-In
     public void manageCheckIn(Guest guest) {
 
         for (Reservation r : HotelDatabase.getReservations()) {
@@ -116,7 +43,7 @@ public class Receptionist extends Staff {
         System.out.println("No valid reservation found for check-in.");
     }
 
-    // ✅ Check-Out
+    // Check-Out
     public void manageCheckOut(Guest guest) {
 
         for (Reservation r : HotelDatabase.getReservations()) {
