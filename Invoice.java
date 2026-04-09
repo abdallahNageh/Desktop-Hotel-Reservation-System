4940d6abbda705334128aedf9c64aec6f43bb972
Eng\Abdallah Nageh <126877787+abdallahNageh@users.noreply.github.com>
2026-04-09 19:01:56 +0200

    Add files via upload

--- Invoice.java
+++ Invoice.java
@@ -1,9 +1,16 @@
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Invoice {
     private int id;
     private double totalAmount;
     private PaymentMethod paymentMethod;
-    private Date paymentdate ;
+    private LocalDate paymentdate ;
 
+    public Invoice(double totalAmount, PaymentMethod paymentMethod, LocalDate paymentdate) {
+        this.totalAmount = totalAmount;
+        this.paymentMethod = paymentMethod;
+        this.paymentdate = paymentdate;
+        this.id +=1 ;
+    }
 }
